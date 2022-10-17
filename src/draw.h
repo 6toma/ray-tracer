564d1e73d20d00ded8988104913bc185ca60a9c9
silentstormm <kazek.cias@gmail.com>
2022-10-17 23:12:26 +0200

    added debug normals

--- src/draw.h
+++ src/draw.h
@@ -19,9 +19,11 @@ void drawExampleOfCustomVisualDebug();
 
 void drawRay(const Ray& ray, const glm::vec3& color = glm::vec3(1.0f));
 
+void drawNormals(const Scene& scene, int interpolationLevel);
+
 void drawAABB(const AxisAlignedBox& box, DrawMode drawMode = DrawMode::Filled, const glm::vec3& color = glm::vec3(1.0f), float transparency = 1.0f);
 
-void drawTriangle (const Vertex& v0, const Vertex& v1, const Vertex& v2 );
+void drawTriangle (const Vertex& v0, const Vertex& v1, const Vertex& v2);
 void drawMesh(const Mesh& mesh);
 void drawSphere(const Sphere& sphere);
 void drawSphere(const glm::vec3& center, float radius, const glm::vec3& color = glm::vec3(1.0f));
