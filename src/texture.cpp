192a9fe9bca8af00de69737630c8a61e0db6fa50
Renyi <jianghurainxwy@gmail.com>
2022-10-30 22:51:48 +0100

    fix crash

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,6 +1,8 @@
 #include "texture.h"
 #include <framework/image.h>
 #include<numbers>
+#include<iostream>
+#include<cmath>
 static const std::filesystem::path dataDirPath { DATA_DIR };
 glm::vec3 acquireTexel(const Image& image, const glm::vec2& texCoord, const Features& features)
 {
@@ -13,12 +15,31 @@ glm::vec3 acquireTexel(const Image& image, const glm::vec2& texCoord, const Feat
 }
 glm::vec3 acquireTexelEnvironment(const Image& image, const glm::vec3& direction, const Features& features)
 {
+    
     float PI = std::numbers::pi;
+    float v,u;
     if (!features.extra.enableEnvironmentMapping)
         return glm::vec3 { 0.0 };
-    float v = std::atan2(direction.y , direction.x) / 2/PI + 0.5;
-    float u = std::asin(direction.z) / PI + 0.5;
+    if (abs(direction.x) < 1e-6) {
+        if (direction.y > 0)
+            v = 0.75;
+        else
+            v = 0.25;
+    }
+    else v = std::atan2l((long double)direction.y , (long double)direction.x) /2.0/ PI + 0.5;
+    if (abs(direction.z + 1) < 1e-6)
+        u = 0;
+    else if (abs(direction.z - 1) < 1e-6)
+        u = 1;
+    else u = std::asin(direction.z) / PI + 0.5;
     int j = floor(u * (image.width - 1) + 0.5);
-    int i = image.height  - floor(v * (image.height - 1) + 0.5);
+    int i = image.height  - floor(v * (image.height - 1) + 0.5) - 1;
+    /*
+    if (i < 0 || i >= image.height || j < 0 || j >= image.width)
+        std::cout << u << " " << v << " " << i << " " << j << " " << image.width << " " << image.height
+                  << " " <<direction.x << " " << direction.y << " " << direction.y / direction.x << " "
+                  << std::atan2l((long double)direction.y, (long double)direction.x) << " " << direction.z << " "
+                  << std::asin(direction.z) << " " << abs(direction.z + 1) 
+                  << "\n ";*/
     return image.pixels[i * image.width + j];
 }
